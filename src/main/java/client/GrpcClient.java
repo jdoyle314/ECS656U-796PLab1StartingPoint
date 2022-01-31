23818e969dff38fa392d970dc6015d7129dd7085
jdoyle314 <35959450+jdoyle314@users.noreply.github.com>
2022-01-31 16:40:47 +0000

    Fixed Error in Print Statement

--- src/main/java/client/GrpcClient.java
+++ src/main/java/client/GrpcClient.java
@@ -147,7 +147,7 @@ public class GrpcClient {
 	System.out.println("Final Answer");
 	System.out.println(A3.getC00()+" "+A3.getC01()+" "+B3.getC00()+" "+B3.getC01());
 	System.out.println(A3.getC10()+" "+A3.getC11()+" "+B3.getC10()+" "+B3.getC11());
-	System.out.println(C3.getC00()+" "+A3.getC01()+" "+D3.getC00()+" "+B3.getC01());
+	System.out.println(C3.getC00()+" "+C3.getC01()+" "+D3.getC00()+" "+D3.getC01());
 	System.out.println(C3.getC10()+" "+C3.getC11()+" "+D3.getC10()+" "+D3.getC11());
         channel.shutdown();
     }
